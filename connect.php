0ece87fa1223abc68db6fe3ece12dbe7ed27b441
Edgard Lorraine Messias <edgardmessias@gmail.com>
2015-08-03 10:07:08 -0300

    Updated;

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 $pdo = new PDO(
-        'informix:host=127.0.0.1;service=9088;database=test;server=dev;protocol=onsoctcp;CLIENT_LOCALE=en_US.utf8;DB_LOCALE=en_US.8859-1;EnableScrollableCursors=1'
+        'informix:host=127.0.0.1;service=9088;database=test;server=dev;protocol=onsoctcp;CLIENT_LOCALE=en_US.utf8;DB_LOCALE=en_US.utf8;EnableScrollableCursors=1'
         , 'test'
         , 'test'
 );
